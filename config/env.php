f320fb8ddf3df44a324f4220060ca494745b2a51
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-06-13 16:50:09 +0530

    Update env.php

--- config/env.php
+++ config/env.php
@@ -2,6 +2,6 @@
 
 //local database
 define('DB_NAME','scms_db');
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('HOST','127.0.0.1');
+define('DB_USER','teamDBMS@team404');
+define('DB_PASSWORD','Abcd1234');
+define('HOST','team404.mysql.database.azure.com');
