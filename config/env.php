824a62294b4807c710316c5a84ea1f2aad69cc77
BanulaMahen <banulakumarage@gmail.com>
2022-06-24 20:13:13 +0530

    added freesql database details

--- config/env.php
+++ config/env.php
@@ -1,8 +1,13 @@
 <?php 
 
-//local database
-define('DB_NAME','scms_db');
-define('DB_USER','teamDBMS@team404');
-define('DB_PASSWORD','Abcd1234');
-define('HOST','team404.mysql.database.azure.com');
+// //azure - local database
+// define('DB_NAME','scms_db');
+// define('DB_USER','teamDBMS@team404');
+// define('DB_PASSWORD','Abcd1234');
+// define('HOST','team404.mysql.database.azure.com');
 
+// freesql - db
+define('DB_NAME','sql6501921');
+define('DB_USER','sql6501921');
+define('DB_PASSWORD','MQwtFjykSx');
+define('HOST','sql6.freesqldatabase.com');
