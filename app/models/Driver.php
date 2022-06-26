7ee70c93044151a109c2b821b727f42f7f812c32
KaveeshaPiumini <piumini95kaveesha@gmail.com>
2022-06-26 16:43:18 +0530

    turn completetion

--- app/models/Driver.php
+++ app/models/Driver.php
@@ -8,4 +8,11 @@ class Driver extends Model {
         parent::__construct($table);
     }
 
+    public function getRemainingOrders($driver_id){
+        $sql = "SELECT COUNT(driver_id) AS CountDriver FROM driver_assistant_order WHERE driver_id = ? ";
+        $resultsQuery = $this->_db->query($sql,[$driver_id]);
+        $result = $resultsQuery->results();
+        return $result[0]-> CountDriver;
+    }
+
 }
