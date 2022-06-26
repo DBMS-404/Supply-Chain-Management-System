11f8a7c03c432b1ffd25305324d58497225fe4e7
BanulaMahen <banulakumarage@gmail.com>
2022-06-26 17:29:07 +0530

    adding navs and footers

--- app/views/stock_manager/manage_item.php
+++ app/views/stock_manager/manage_item.php
@@ -1,10 +1,12 @@
 <?php
+$page = "Add Item";
 if ($_POST) {
     $values = posted_values($_POST);
 }
 
 if (isset($this->edit_values)) {
     $values = $this->edit_values;
+    $page = "Edit item";
 }
 ?>
 
@@ -15,32 +17,49 @@ if (isset($this->edit_values)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
     <title>Manage Stock</title>
     <style>
-        .bg-danger {
-            color: #FF0000;
-        }
-
-        .Appcontainer {
-            z-index: 2;
-            border-radius: 15px;
-            background-color: #e9e9e9ed;
-            height: 15%;
-            width: 40%;
-            margin: auto;
-            margin-top: 2cm;
-            padding: 30px;
-            padding-left: 30px;
-            padding-right: 30px;
-            box-shadow: 10px 10px 50px 0.1px rgba(0, 0, 0, 0.664);
-        }
+        <?php include_once('assets/bootstrap/css/bootstrap.min.css'); ?>
     </style>
-    <?php include_once('css/baseForm.php'); ?>
 </head>
 
-<body>
-    <div class="container-fluid">
-        <h1 class="header" style="text-align: center;">Manage stock</h1>
+<body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
+    <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="/">
+                <span class="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon"><img class="img-fluid" src="assets/img/logo-modified.png"></span><span>Supply Chain Management System</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav mx-auto">
+                <li class="nav-item"><a class="nav-link active" href="<?=SROOT?>StockManagerHandler/viewinventory">Inventory</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?=SROOT?>StockManagerHandler/vieworders">Orders</a></li>
+                </ul><a class="btn btn-primary btn-sm shadow" role="button" href="signup.html">Logout</a>
+            </div>
+        </div>
+    </nav>
+    <header class="bg-primary-gradient pt-5">
+        <div class="container pt-4 pt-xl-5">
+            <div class="row pt-5">
+                <div class="col-md-8 col-xl-6 text-center text-md-start mx-auto">
+                    <div class="text-center">
+                        <p class="fw-bold text-success mb-2">Voted #1 Worldwide</p>
+                        <h1 class="fw-bold">The best solution for you and your customers</h1>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-10 mx-auto">
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+        <nav aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="<?=SROOT?>StockManagerHandler">Home</a></li>
+                <li class="breadcrumb-item"><a href="<?=SROOT?>StockManagerHandler/viewinventory">Inventory</a></li>
+                <li class="breadcrumb-item active" aria-current="page"><?=$page?></li>
+            </ol>
+        </nav>
+        <h1>Manage stock</h1>
         <div class="Appcontainer">
             <form class="form-horizontal" action="<?= SROOT ?>StockManagerHandler/manageStock/<?= $this->item_id ?>" method="post">
                 <label>Item Name: </label>
@@ -57,6 +76,22 @@ if (isset($this->edit_values)) {
             </span>
         </div>
     </div>
+    <footer class="bg-primary-gradient">
+        <div class="container py-4 ">
+            <div class="text-muted d-flex justify-content-between align-items-center">
+                <p class="mb-0">Copyright © 2022 Team 404</p>
+            </div>
+        </div>
+    </footer>
+    <script>
+        <?php include_once('assets/js/jquery.min.js'); ?>
+    </script>
+    <script>
+        <?php include_once('assets/bootstrap/js/bootstrap.min.js'); ?>
+    </script>
+    <script>
+        <?php include_once('assets/js/bold-and-bright.js'); ?>
+    </script>
 </body>
 
 </html>
--- app/views/stock_manager/train_assignment.php
+++ app/views/stock_manager/train_assignment.php
@@ -5,11 +5,46 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Train Assignment</title>
-    <?php include_once('css/baseTable.php'); ?>
-
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <style>
+        <?php include_once('assets/bootstrap/css/bootstrap.min.css'); ?>
+    </style>
 </head>
-<body>
-    <div class="container-fluid">
+<body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
+<nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand d-flex align-items-center" href="/">
+                <span class="bs-icon-sm bs-icon-circle bs-icon-primary shadow d-flex justify-content-center align-items-center me-2 bs-icon"><img class="img-fluid" src="assets/img/logo-modified.png"></span><span>Supply Chain Management System</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="navbar-nav mx-auto">
+                    <li class="nav-item"><a class="nav-link" href="<?=SROOT?>StockManagerHandler/viewinventory">Inventory</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="<?=SROOT?>StockManagerHandler/vieworders">Orders</a></li>
+                </ul><a class="btn btn-primary btn-sm shadow" role="button" href="signup.html">Logout</a>
+            </div>
+        </div>
+    </nav>
+    <header class="bg-primary-gradient pt-5">
+        <div class="container pt-4 pt-xl-5">
+            <div class="row pt-5">
+                <div class="col-md-8 col-xl-6 text-center text-md-start mx-auto">
+                    <div class="text-center">
+                        <p class="fw-bold text-success mb-2">Voted #1 Worldwide</p>
+                        <h1 class="fw-bold">The best solution for you and your customers</h1>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-10 mx-auto">
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="<?=SROOT?>StockManagerHandler">Home</a></li>
+            <li class="breadcrumb-item"><a href="<?=SROOT?>StockManagerHandler/vieworders">Orders</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Train assignment</li>
+        </ol>
+    </nav>
     <div>
         <h2>Order Weight: <?=$this->order->weight." kg"?></h2>
     </div>
@@ -45,5 +80,21 @@
         </table>
     </div>
     </div>
+    <footer class="bg-primary-gradient">
+        <div class="container py-4 ">
+            <div class="text-muted d-flex justify-content-between align-items-center">
+                <p class="mb-0">Copyright © 2022 Team 404</p>
+            </div>
+        </div>
+    </footer>
+    <script>
+        <?php include_once('assets/js/jquery.min.js'); ?>
+    </script>
+    <script>
+        <?php include_once('assets/bootstrap/js/bootstrap.min.js'); ?>
+    </script>
+    <script>
+        <?php include_once('assets/js/bold-and-bright.js'); ?>
+    </script>
 </body>
 </html>
