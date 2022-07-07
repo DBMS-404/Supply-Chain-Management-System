8035dd5dd43aec7ceafdd14c065bf16094221c2b
BanulaMahen <banulakumarage@gmail.com>
2022-07-07 12:32:32 +0530

    manager navbar updated

--- app/Views/manager/itemsReport.php
+++ app/Views/manager/itemsReport.php
@@ -1,5 +1,5 @@
-<?php 
-    redirectToHandler('mn');
+<?php
+redirectToHandler('mn');
 ?>
 
 <!DOCTYPE html>
@@ -20,15 +20,15 @@
 <body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
     <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3" id="mainNav">
         <div class="container">
-            <a class="navbar-brand d-flex align-items-center" href="/">
+            <a class="navbar-brand d-flex align-items-center" href="<?= SROOT ?>LoginHandler/redirectToHandler">
                 <span class="bs-icon-sm bs-icon-circle   shadow d-flex justify-content-center align-items-center me-2 bs-icon"><img class="img-fluid" src="https://static.wixstatic.com/media/dcfc03_6c7b355ab8c0449c9583b19c1badbeb1~mv2.png/v1/fill/w_338,h_328,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Artboard%207%20copy%203.png"></span><span>Supply Chain Management System</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a class="nav-link" href="<?= SROOT ?>ManagerHandler/viewTrainSchedule">Train Schedule</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="<?= SROOT?>ManagerHandler/generateReport">Reports</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="<?= SROOT ?>ManagerHandler/generateReport">Reports</a></li>
                 </ul>
                 <a class="fw-light fs-5"><span style="margin-right: 5px;"><i class="fa fa-user" aria-hidden="true"></i> <?php print_r($_SESSION['user_id']); ?></span></a>
-                <div  class="d-lg-none mb-3"></div>
+                <div class="d-lg-none mb-3"></div>
                 <a class="btn btn-primary btn-sm shadow" role="button" href="<?= SROOT ?>LoginHandler/logout">Logout</a>
             </div>
         </div>
@@ -52,7 +52,7 @@
         <div class="row m-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item active"><a href="<?= SROOT?>ManagerHandler/generateReport">Generate Reports</a></li>
+                    <li class="breadcrumb-item active"><a href="<?= SROOT ?>ManagerHandler/generateReport">Generate Reports</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Item Report</li>
                 </ol>
             </nav>
@@ -67,75 +67,80 @@
                         <input class="btn-sm btn-success" type="submit" value="->">
                     </div>
                 </div>
-            </div>  
+            </div>
         </form>
         <br>
 
-        <div class="row">        
+        <div class="row">
             <div class="row m-2">
                 <h3>Items with most orders</h3>
-                <h5 class='text-muted'>Time Range : <?php if($this->first_date=="" and $this->second_date==""){?>All Time   
-                                        <?php }elseif($this->first_date=="" and $this->second_date!=""){?>Until <?=$this->second_date?>
-                                        <?php }elseif($this->first_date!="" and $this->second_date==""){?>From <?=$this->first_date?>
-                                        <?php }else{?>From <?=$this->first_date?> to <?=$this->second_date?>
-                                        <?php }?> </h5>
-            <?php if(isset($this->displayErrors)){?>
-                <br><br>
-                <span class="text-danger"><h5><?=$this->displayErrors?></h5></span>
-                    </div>
-                </div>
-
-            <?php } else {?>
-                <?php if (count($this->items) > 0) {$x=0 ?>
-                    <div class="row">
-                        <?php foreach ($this->items as $item) {$x++ 
-                        ?>
-                            <div class="col-sm-4 col-12 mt-3 mb-3">
-                                <div class="card bg-light shadow-lg">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-sm-9">
-                                                <h5 class="card-title">Item : <?= $item->name ?></button></h5>
-                                                <h6 class="card-subtitle mb-2 text-muted">Item ID : <?= $item->item_id?></h6>
-                                            </div>
-                                            <div class="col-sm-3">
-                                                <h4><span class="badge bg-danger"><?= $x ?></span></h4>
-                                            </div>
-                                        </div>
-                                        
-                                        <hr>
-                                        <!-- <h6 class="card-title"><button type='button' class='btn btn-danger'><?= $x ?></button></h5> -->
-                                        <p class="card-text"><?= "Sold amount : " . $item->item_count ?><br>
-                                        <?= "Unit price : Rs." . $item->unit_price ?>
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        <?php } ?>
-                        <?php } else { ?>
-                            <h5>No Items are sold in the selected Time Period</h5>
-                        <?php } ?>
-
-                    </div>
-                </div>
-            </div>
-        <?php } ?>
-    </div>
-    <footer class="bg-primary-gradient">
-        <div class="container py-4 ">
-            <div class="text-muted d-flex justify-content-between align-items-center">
-                <p class="mb-0">Copyright © 2022 Team 404</p>
+                <h5 class='text-muted'>Time Range : <?php if ($this->first_date == "" and $this->second_date == "") { ?>All Time
+                    <?php } elseif ($this->first_date == "" and $this->second_date != "") { ?>Until <?= $this->second_date ?>
+                    <?php } elseif ($this->first_date != "" and $this->second_date == "") { ?>From <?= $this->first_date ?>
+                    <?php } else { ?>From <?= $this->first_date ?> to <?= $this->second_date ?>
+                <?php } ?> </h5>
+                <?php if (isset($this->displayErrors)) { ?>
+                    <br><br>
+                    <span class="text-danger">
+                        <h5><?= $this->displayErrors ?></h5>
+                    </span>
             </div>
         </div>
-    </footer>
-    <script>
-        <?php include_once('assets/js/jquery.min.js'); ?>
-    </script>
-    <script>
-        <?php include_once('assets/bootstrap/js/bootstrap.min.js'); ?>
-    </script>
-    <script>
-        <?php include_once('assets/js/bold-and-bright.js'); ?>
-    </script>
+
+    <?php } else { ?>
+        <?php if (count($this->items) > 0) {
+                        $x = 0 ?>
+            <div class="row">
+                <?php foreach ($this->items as $item) {
+                            $x++
+                ?>
+                    <div class="col-sm-4 col-12 mt-3 mb-3">
+                        <div class="card bg-light shadow-lg">
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-sm-9">
+                                        <h5 class="card-title">Item : <?= $item->name ?></button></h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">Item ID : <?= $item->item_id ?></h6>
+                                    </div>
+                                    <div class="col-sm-3">
+                                        <h4><span class="badge bg-danger"><?= $x ?></span></h4>
+                                    </div>
+                                </div>
+
+                                <hr>
+                                <!-- <h6 class="card-title"><button type='button' class='btn btn-danger'><?= $x ?></button></h5> -->
+                                <p class="card-text"><?= "Sold amount : " . $item->item_count ?><br>
+                                    <?= "Unit price : Rs." . $item->unit_price ?>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            <?php } else { ?>
+                <h5>No Items are sold in the selected Time Period</h5>
+            <?php } ?>
+
+            </div>
+    </div>
+    </div>
+<?php } ?>
+</div>
+<footer class="bg-primary-gradient">
+    <div class="container py-4 ">
+        <div class="text-muted d-flex justify-content-between align-items-center">
+            <p class="mb-0">Copyright © 2022 Team 404</p>
+        </div>
+    </div>
+</footer>
+<script>
+    <?php include_once('assets/js/jquery.min.js'); ?>
+</script>
+<script>
+    <?php include_once('assets/bootstrap/js/bootstrap.min.js'); ?>
+</script>
+<script>
+    <?php include_once('assets/js/bold-and-bright.js'); ?>
+</script>
 </body>
+
 </html>
--- app/Views/manager/sales_report.php
+++ app/Views/manager/sales_report.php
@@ -1,5 +1,5 @@
-<?php 
-    redirectToHandler('mn');
+<?php
+redirectToHandler('mn');
 ?>
 
 <!DOCTYPE html>
@@ -20,15 +20,15 @@
 <body style="/*background: url(&quot;design.jpg&quot;);*/background-position: 0 -60px;">
     <nav class="navbar navbar-light navbar-expand-md fixed-top navbar-shrink py-3" id="mainNav">
         <div class="container">
-            <a class="navbar-brand d-flex align-items-center" href="/">
+            <a class="navbar-brand d-flex align-items-center" href="<?= SROOT ?>LoginHandler/redirectToHandler">
                 <span class="bs-icon-sm bs-icon-circle   shadow d-flex justify-content-center align-items-center me-2 bs-icon"><img class="img-fluid" src="https://static.wixstatic.com/media/dcfc03_6c7b355ab8c0449c9583b19c1badbeb1~mv2.png/v1/fill/w_338,h_328,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Artboard%207%20copy%203.png"></span><span>Supply Chain Management System</span></a><button data-bs-toggle="collapse" class="navbar-toggler" data-bs-target="#navcol-1"><span class="visually-hidden">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item"><a class="nav-link" href="<?= SROOT ?>ManagerHandler/viewTrainSchedule">Train Schedule</a></li>
-                    <li class="nav-item"><a class="nav-link active" href="<?= SROOT?>ManagerHandler/generateReport">Reports</a></li>
+                    <li class="nav-item"><a class="nav-link active" href="<?= SROOT ?>ManagerHandler/generateReport">Reports</a></li>
                 </ul>
                 <a class="fw-light fs-5"><span style="margin-right: 5px;"><i class="fa fa-user" aria-hidden="true"></i> <?php print_r($_SESSION['user_id']); ?></span></a>
-                <div  class="d-lg-none mb-3"></div>
+                <div class="d-lg-none mb-3"></div>
                 <a class="btn btn-primary btn-sm shadow" role="button" href="<?= SROOT ?>LoginHandler/logout">Logout</a>
             </div>
         </div>
@@ -52,67 +52,67 @@
         <div class="row m-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb">
-                    <li class="breadcrumb-item active"><a href="<?= SROOT?>ManagerHandler/generateReport">Generate Reports</a></li>
+                    <li class="breadcrumb-item active"><a href="<?= SROOT ?>ManagerHandler/generateReport">Generate Reports</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Sales Report</li>
                 </ol>
             </nav>
         </div>
-            <form class="p-3 p-xl-4" action="<?= SROOT ?>ManagerHandler/generateReport/4" method="post">
-                <div class='row mb-2'>
-                    <div class=col-sm-2>
-                        <label for="time_period">Select the Time Period</label>
-                    </div>
-                    <div class=col-sm-2>
-                        <select class="form-select"  name='time_period'>
-                            <option value="0" selected>All Time</option>
-                            <option value="1">1st Quarter</option>
-                            <option value="2">2nd Quarter</option>
-                            <option value="3">3rd Quarter</option>
-                            <option value="4">4th Quarter</option>
-                        </select>
-                    </div>
+        <form class="p-3 p-xl-4" action="<?= SROOT ?>ManagerHandler/generateReport/4" method="post">
+            <div class='row mb-2'>
+                <div class=col-sm-2>
+                    <label for="time_period">Select the Time Period</label>
                 </div>
-                <div class='row mb-2'>
-                    <div class=col-sm-2>
-                        <label for="city">Select the City</label>
-                    </div>
-                    <div class=col-sm-2>
-                        <select class="form-select"  name='city'>
-                            <option value="0-All Cities" selected>All cities</option>
-                            <?php foreach($this->cities as $city){ ?>
-                                <option value="<?= $city->city_id?>-<?= $city->name ?>"><?= $city->name ?></option>
-                            <?php }?>
-                        </select>
-                    </div>
-                    <div class=col-sm-2>
-                        <label for="route">Select the Route</label>
-                    </div>
-                    <div class=col-sm-2>
-                        <select class="form-select"  name='route'>
-                            <option value="0" selected>All routes</option>
-                            <?php foreach($this->routes as $route){ ?>
-                                <option value="<?= $route->route_id ?>">Route <?= $route->route_id ?></option>
-                            <?php }?>
-                        </select>
-                    </div>
+                <div class=col-sm-2>
+                    <select class="form-select" name='time_period'>
+                        <option value="0" selected>All Time</option>
+                        <option value="1">1st Quarter</option>
+                        <option value="2">2nd Quarter</option>
+                        <option value="3">3rd Quarter</option>
+                        <option value="4">4th Quarter</option>
+                    </select>
                 </div>
-                
-                <div class='row mb-2'>
-                    <div class=col-sm-3>
-                        <input class="btn-sm btn-success" type="submit" value="Search">
-                    </div>
+            </div>
+            <div class='row mb-2'>
+                <div class=col-sm-2>
+                    <label for="city">Select the City</label>
                 </div>
-            </form>
+                <div class=col-sm-2>
+                    <select class="form-select" name='city'>
+                        <option value="0-All Cities" selected>All cities</option>
+                        <?php foreach ($this->cities as $city) { ?>
+                            <option value="<?= $city->city_id ?>-<?= $city->name ?>"><?= $city->name ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+                <div class=col-sm-2>
+                    <label for="route">Select the Route</label>
+                </div>
+                <div class=col-sm-2>
+                    <select class="form-select" name='route'>
+                        <option value="0" selected>All routes</option>
+                        <?php foreach ($this->routes as $route) { ?>
+                            <option value="<?= $route->route_id ?>">Route <?= $route->route_id ?></option>
+                        <?php } ?>
+                    </select>
+                </div>
+            </div>
 
-        <div class="row">        
+            <div class='row mb-2'>
+                <div class=col-sm-3>
+                    <input class="btn-sm btn-success" type="submit" value="Search">
+                </div>
+            </div>
+        </form>
+
+        <div class="row">
             <div class="row m-2">
                 <h3>Sales Report</h3>
                 <br><br>
-                <h5 class='text-muted'>Time Range : <?php if($this->first_date=="" and $this->second_date==""){?>All Time
-                                        <?php }else{?>Quarter<?= " ".$this->time_period." "?> || From <?=$this->first_date?> to <?=$this->second_date?>
-                                        <?php }?> </h5>
-                <h5 class='text-muted'>Route : <?= $this->route?> </h5>
-                <h5 class='text-muted'>City : <?= $this->city?> </h5><br>
+                <h5 class='text-muted'>Time Range : <?php if ($this->first_date == "" and $this->second_date == "") { ?>All Time
+                    <?php } else { ?>Quarter<?= " " . $this->time_period . " " ?> || From <?= $this->first_date ?> to <?= $this->second_date ?>
+                <?php } ?> </h5>
+                <h5 class='text-muted'>Route : <?= $this->route ?> </h5>
+                <h5 class='text-muted'>City : <?= $this->city ?> </h5><br>
                 <table class="table">
                     <tbody id="trains">
                         <hr>
@@ -121,23 +121,23 @@
                                 <tr class="order-id" style="width: 807.2px;">
                                     <div class="row">
                                         <div class="col-sm-9">
-                                            <p class="fw-bolder fs-5"><?= "Order ID - " . $order->order_id?></p>
+                                            <p class="fw-bolder fs-5"><?= "Order ID - " . $order->order_id ?></p>
                                         </div>
                                         <div class="col-sm-3 text-muted">
-                                            <?= "Scheduled Date - ". $order->date ?><br>
+                                            <?= "Scheduled Date - " . $order->date ?><br>
                                         </div>
                                     </div>
                                     <div class="row">
                                         <div class="col-sm-8">
-                                            <?= "Customer - ". $order->user_id." || ".$order->first_name." ".$order->last_name ?><br>
-                                            <?= "Status - ".$order->status ?><br>
+                                            <?= "Customer - " . $order->user_id . " || " . $order->first_name . " " . $order->last_name ?><br>
+                                            <?= "Status - " . $order->status ?><br>
                                         </div>
                                         <div class="col-sm-2">
-                                            <h5><span class="badge bg-success p-2"><?= "Weight - ".$order->weight." g"?></span></h5><br>
+                                            <h5><span class="badge bg-success p-2"><?= "Weight - " . $order->weight . " g" ?></span></h5><br>
                                         </div>
                                         <div class="col-sm-2">
-                                            <h5><span class="badge bg-danger p-2"><?= "Total Price - Rs.".$order->total_price?></span></h5><br>
-                                        </div>  
+                                            <h5><span class="badge bg-danger p-2"><?= "Total Price - Rs." . $order->total_price ?></span></h5><br>
+                                        </div>
                                     </div>
                                 </tr>
                                 <hr>
@@ -147,11 +147,13 @@
                         <?php } ?>
                     </tbody>
                 </table>
-            <?php if(isset($this->displayErrors)){?>
-                <br><br>
-                <span class="text-danger"><h5><?=$this->displayErrors?></h5></span>
-            <?php } else {?>  
-        <?php } ?>
+                <?php if (isset($this->displayErrors)) { ?>
+                    <br><br>
+                    <span class="text-danger">
+                        <h5><?= $this->displayErrors ?></h5>
+                    </span>
+                <?php } else { ?>
+                <?php } ?>
             </div>
         </div>
 
@@ -173,4 +175,5 @@
         <?php include_once('assets/js/bold-and-bright.js'); ?>
     </script>
 </body>
+
 </html>
